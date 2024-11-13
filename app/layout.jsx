0bdc6bfafbd9ebad586b6b90d90ee8fab1db9e02
raensh33sh <randykamalhusen@gmail.com>
2024-11-13 13:51:25 +0800

    memulai part ini

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,22 @@
+'use client'
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from './components/Header'
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function RootLayout({
-  children,
+  children
 }){
+
+  const path = usePathname()
+  const red = useRouter()
+  // buat redirect kalo udah login ga bisa akses page tertentu
+  const isAuthenticated = false
+  if (isAuthenticated && (path === '/login' || path === '/daftar')) {
+    red.push('/')
+  }
+
   return (
     <html lang="en">
       <head>
