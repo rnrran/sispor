ed197c8f49c56e8abe88d51e5fb54b3282cc35c0
raensh33sh <randykamalhusen@gmail.com>
2024-11-13 19:51:26 +0800

    kayanya final ini buat fitur regis/login

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -63,10 +63,9 @@ const Header = () => {
                                     tabIndex={0}
                                     className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                                     <li>
-                                    <a className="justify-between">
+                                    <Link href="/profile"className="justify-between">
                                         Profile
-                                        <span className="badge">New</span>
-                                    </a>
+                                    </Link>
                                     </li>
                                     <li><a>Settings</a></li>
                                     <li><button onClick={handleLogout}>Logout</button></li>
